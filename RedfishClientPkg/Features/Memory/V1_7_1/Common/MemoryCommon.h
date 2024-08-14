a09fd42735fb9f98915b1c7fa1a15d14c688d706
Nickle Wang <nicklew@nvidia.com>
2024-08-14 22:09:57 +0800

    RedfishClientPkg/Memory: rename Redpath to ConfigLang
    To prevent confusion, Redpath is renamed to configure language in ReadMe.
    So, having this change to rename "Redpath" to "ConfigLang" in source
    code.
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>

--- RedfishClientPkg/Features/Memory/V1_7_1/Common/MemoryCommon.h
+++ RedfishClientPkg/Features/Memory/V1_7_1/Common/MemoryCommon.h
@@ -2,6 +2,7 @@
 
   Redfish feature driver implementation - internal header file
   (C) Copyright 2020-2021 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -15,13 +16,13 @@
 //
 // Schema information.
 //
-#define RESOURCE_SCHEMA          "Memory"
-#define RESOURCE_SCHEMA_MAJOR    "1"
-#define RESOURCE_SCHEMA_MINOR    "7"
-#define RESOURCE_SCHEMA_ERRATA   "1"
-#define RESOURCE_SCHEMA_VERSION  "v1_7_1"
-#define REDPATH_ARRAY_PATTERN    L"/Memory/\\{.*\\}/"
-#define REDPATH_ARRAY_PREFIX     L"/Memory/"
-#define RESOURCE_SCHEMA_FULL     "x-UEFI-redfish-Memory.v1_7_1"
+#define RESOURCE_SCHEMA            "Memory"
+#define RESOURCE_SCHEMA_MAJOR      "1"
+#define RESOURCE_SCHEMA_MINOR      "7"
+#define RESOURCE_SCHEMA_ERRATA     "1"
+#define RESOURCE_SCHEMA_VERSION    "v1_7_1"
+#define CONFIG_LANG_ARRAY_PATTERN  L"/Memory/\\{.*\\}/"
+#define CONFIG_LANG_ARRAY_PREFIX   L"/Memory/"
+#define RESOURCE_SCHEMA_FULL       "x-UEFI-redfish-Memory.v1_7_1"
 
 #endif
