1c70dd38e52e8e8fe00b1f561a41158e9ceb4272
Nickle Wang <nicklew@nvidia.com>
2024-08-14 22:08:37 +0800

    RedfishClientPkg/Bios: rename Redpath to ConfigLang
    To prevent confusion, Redpath is renamed to configure language in ReadMe.
    So, having this change to rename "Redpath" to "ConfigLang" in source
    code.
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>

--- RedfishClientPkg/Features/Bios/v1_0_9/Common/BiosCommon.h
+++ RedfishClientPkg/Features/Bios/v1_0_9/Common/BiosCommon.h
@@ -2,6 +2,7 @@
 
   Redfish feature driver implementation - internal header file
   (C) Copyright 2020-2022 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -15,16 +16,15 @@
 //
 // Schema information.
 //
-#define REDFISH_MANAGED_URI      L"Systems/{}/Bios"
-#define MAX_URI_LENGTH           256
-#define RESOURCE_SCHEMA          "Bios"
-#define RESOURCE_SCHEMA_MAJOR    "1"
-#define RESOURCE_SCHEMA_MINOR    "0"
-#define RESOURCE_SCHEMA_ERRATA   "9"
-#define RESOURCE_SCHEMA_VERSION  "v1_0_9"
-#define REDPATH_ARRAY_PATTERN    L"/Bios/.*"
-#define REDPATH_ARRAY_PREFIX     L"/Bios/"
-#define RESOURCE_SCHEMA_FULL     "x-UEFI-redfish-Bios.v1_0_9"
-#define REDFISH_SCHEMA_NAME      "ComputerSystem"
+#define REDFISH_MANAGED_URI        L"Systems/{}/Bios"
+#define MAX_URI_LENGTH             256
+#define RESOURCE_SCHEMA            "Bios"
+#define RESOURCE_SCHEMA_MAJOR      "1"
+#define RESOURCE_SCHEMA_MINOR      "0"
+#define RESOURCE_SCHEMA_ERRATA     "9"
+#define RESOURCE_SCHEMA_VERSION    "v1_0_9"
+#define CONFIG_LANG_ARRAY_PATTERN  L"/Bios/.*"
+#define CONFIG_LANG_ARRAY_PREFIX   L"/Bios/"
+#define RESOURCE_SCHEMA_FULL       "x-UEFI-redfish-Bios.v1_0_9"
 
 #endif
