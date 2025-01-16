eee53bc3f6d818b58a8e9c891bc393aedcbe7583
Nickle Wang <nicklew@nvidia.com>
2025-01-16 09:42:45 +0800

    RedfishClientPkg/BootOptionCollection: default detection improvement
    When system loads default and clear variables, feature driver should
    do a provision and push default values to BMC. And feature driver
    will ignore any pending settings because they are made based on
    stale settings.
    
    However, instead of provision(), update() is called in feature driver.
    Update() may not push settings to BMC when BMC already has same settings.
    
    Use provision() to force update default settings to BMC, so BMC has
    chance to reset resource like /Settings and /SD or attribute like
    ResetBiosToDefaultsPending.
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>

--- RedfishClientPkg/Features/BootOptionCollection/BootOptionCollectionDxe.c
+++ RedfishClientPkg/Features/BootOptionCollection/BootOptionCollectionDxe.c
@@ -3,7 +3,7 @@
   Redfish feature driver implementation - BootOptionCollection
 
   (C) Copyright 2020-2022 Hewlett Packard Enterprise Development LP<BR>
-  Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+  Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -32,7 +32,6 @@ HandleResource (
   EFI_STATUS           Status;
   REDFISH_SCHEMA_INFO  SchemaInfo;
   EFI_STRING           ConfigLang;
-  BOOLEAN              SystemRestDetected;
 
   if ((Private == NULL) || IS_EMPTY_STRING (Uri)) {
     return EFI_INVALID_PARAMETER;
@@ -55,8 +54,7 @@ HandleResource (
   // Some resource is handled by other provider so we have to make sure this first.
   //
   DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a: Identify for %s\n", __func__, Uri));
-  SystemRestDetected = FALSE;
-  ConfigLang         = RedfishGetConfigLanguage (Uri);
+  ConfigLang = RedfishGetConfigLanguage (Uri);
   if (ConfigLang == NULL) {
     Status = EdkIIRedfishResourceConfigIdentify (&SchemaInfo, Uri, NULL, Private->InformationExchange);
     if (EFI_ERROR (Status)) {
@@ -77,26 +75,28 @@ HandleResource (
     // system is reset by defaulting command. The pending setting on BMC may be
     // a stale value so we will ignore pending settings in BMC.
     //
-    SystemRestDetected = TRUE;
+    DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a: system has been reset to default setting. Ignore pending settings because they may be stale values\n", __func__));
   } else {
     DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a: history record found: %s\n", __func__, ConfigLang));
     FreePool (ConfigLang);
   }
 
   //
-  // Check and see if target property exist or not even when collection member exists.
-  // If not, we still do provision.
+  // Perform provisioning in two cases:
+  // 1) System is reset by defaulting command.
+  // 2) Check and see if target property exist or not even when collection member exists.
+  //    If not, we will do provision.
   //
   DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a Check for %s\n", __func__, Uri));
   Status = EdkIIRedfishResourceConfigCheck (&SchemaInfo, Uri, NULL);
-  if (EFI_ERROR (Status)) {
+  if ((ConfigLang == NULL) || EFI_ERROR (Status)) {
     if (Status == EFI_UNSUPPORTED) {
       DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a: \"%s\" is not handled by us\n", __func__, Uri));
       return EFI_ABORTED;
     }
 
     //
-    // The target property does not exist, do the provision to create property.
+    // The target property does not exist or stale, do the provision to create property.
     //
     DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a provision for %s\n", __func__, Uri));
     Status = EdkIIRedfishResourceConfigProvisioning (&SchemaInfo, Uri, NULL, Private->InformationExchange, FALSE);
@@ -110,14 +110,10 @@ HandleResource (
   //
   // Consume first.
   //
-  if (SystemRestDetected) {
-    DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a system has been reset to default setting. ignore pending settings because they may be stale values\n", __func__));
-  } else {
-    DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a consume for %s\n", __func__, Uri));
-    Status = EdkIIRedfishResourceConfigConsume (&SchemaInfo, Uri, NULL);
-    if (EFI_ERROR (Status)) {
-      DEBUG ((DEBUG_ERROR, "%a: failed to consume resource for: %s: %r\n", __func__, Uri, Status));
-    }
+  DEBUG ((REDFISH_BOOT_OPTION_COLLECTION_DEBUG_TRACE, "%a consume for %s\n", __func__, Uri));
+  Status = EdkIIRedfishResourceConfigConsume (&SchemaInfo, Uri, NULL);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: failed to consume resource for: %s: %r\n", __func__, Uri, Status));
   }
 
   //
